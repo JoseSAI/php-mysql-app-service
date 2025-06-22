54d37df99e074dea2163d3811f69936e2d8e6db2
JoseSAI <j.pratsbeltran@edu.gva.es>
2025-06-22 11:17:12 +0200

    Update header.php

--- templates/header.php
+++ templates/header.php
@@ -5,11 +5,11 @@
 	<meta charset="utf-8">
 	<meta http-equiv="x-ua-compatible" content="ie=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>Product Catalog</title>
+	<title>Catálogo de Productos</title>
 	<link rel="stylesheet" href="css/style.css">
 </head>
 <body>
 	<div class="center-align"> 
-		<h1>Product Catalog</h1>
+		<h1>Catálogo/h1>
 		<br>
 	</div>
